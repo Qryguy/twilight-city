a97fbe7f57555a53b6cbe4d313ef6fe5c0ddc461
Q <sheeranrye@gmail.com>
2023-06-12 22:47:55 -0500

    Fixed menus!
    Fixed pause menu to open/close on button press. Fixed broken mission failed  screen

--- twilight-city/scripts/gameOverMenu.gd
+++ twilight-city/scripts/gameOverMenu.gd
@@ -1,13 +1,15 @@
 extends CanvasLayer
 
-var currentLevel = get_tree().get_current_scene()
+#var currentLevel = get_tree().get_current_scene()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func _on_restart_level_pressed():
-	get_tree().change_scene_to_packed(currentLevel)
+	pass
+	get_tree().reload_current_scene()
+	#get_tree().change_scene_to_packed(currentLevel)
 
 func _on_main_menu_pressed():
 	get_tree().change_scene_to_file("res://twilight-city/scenes/menus/mainMenu.tscn")
--- twilight-city/scripts/pauseMenu.gd
+++ twilight-city/scripts/pauseMenu.gd
@@ -1,29 +1,21 @@
-extends Control
-
-#no idea how we get this yet, this will be a Scene data type
-#will this work? currentScene = get_tree().get_current_scene() (.get_filename for file naming)
-#var currentLevel
-
-func _input(event):
-	get_tree().paused = true
-
-func _ready():
-	#I think this will be needed to set the level to "paused"
-	pass # Replace with function body.
+extends CanvasLayer
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	#don't think we need process
-	pass
+	if Input.is_action_just_pressed("back_menu"):
+		var new_paused_state = not get_tree().paused
+		get_tree().paused = new_paused_state
+		visible = new_paused_state
 
 func _on_resume_pressed():
-	#close this menu and return to the game, unsure if we need the current level value or not
-	pass # Replace with function body.
+	#same function as pressing menu again
+	var new_paused_state = not get_tree().paused
+	get_tree().paused = new_paused_state
+	visible = new_paused_state
 
 func _on_options_pressed():
 	#open the options menu on top of this
-	#options will need to track which menu it came from, hoping we can instantiate it on top of the current menu and just close it when exiting
-	#thinking about it I hope we can use the same logic for this pause menu: open it, stop gameplay; close it, continue gameplay (or switch those probably)
+	#options will need to track which menu it came from, think we can do the same as how pause menu is put over the level, toggle visibility of a child node in this scene
 	pass # Replace with function body.
 
 func _on_main_pressed():
@@ -33,6 +25,3 @@ func _on_main_pressed():
 func _on_quit_pressed():
 	#save prompt? if we have anything that saves data
 	get_tree().quit()
-
-
-
