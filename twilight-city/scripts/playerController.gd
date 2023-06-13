3ec68bc2118827c244a61f2548e9d2fdbf4d3020
Q <sheeranrye@gmail.com>
2023-06-12 22:59:19 -0500

    Menu fixes to main + added gitignore

--- twilight-city/scripts/playerController.gd
+++ twilight-city/scripts/playerController.gd
@@ -1,11 +1,13 @@
 extends CharacterBody2D
+class_name playerController
 #probably should have just made these separate scripts but whatever
 @export var unitName: String
 @export var speed = 300.0
-@export var health = 1
+@export var health = 1;
+
+@onready var gameOverScreen: CanvasLayer = get_node("%Game Over Screen")
 
 var playerControl: String
-
 
 func _ready():
 	#set unit controls and settings, set brawn as default? to avoid annoying errors
@@ -15,18 +17,17 @@ func _ready():
 		playerControl = "player1"
 	if unitName == "brain":
 		playerControl = "player2"
-	print(playerControl)
+	print(gameOverScreen.visible)
 
-#process for standard inputs, actually should probably change this to some kind of collider?
+#process updates on every frame
 func _process(_delta):
-	_input_map(unitName)
 	if health == 0:
 		#gameover
-		get_tree().change_scene_to_file("res://twilight-city/scenes/menus/gameOver.tscn")
+		gameOverScreen.visible = true
 
 #physics for movement
 func _physics_process(_delta):
-	##use delta for time based 
+	##use delta for time based physics e.g. gravity
 	var xDirection = Input.get_axis(playerControl+"_left", playerControl+"_right")
 	if xDirection:
 		velocity.x = xDirection * speed
@@ -40,19 +41,3 @@ func _physics_process(_delta):
 		velocity.y = move_toward(velocity.y, 0, speed)
 
 	move_and_slide() #seems to apply the actual movement
-
-func _input_map(unit):
-	#have this var be the input key instead?
-	#var keyPress =
-	if Input.is_action_just_pressed("back_menu"):
-		#sets the pause menu, but does this get called...might need to move this out of the player character?
-		#not really sure the best way to do this, I might have approached this poorly from the start as well.
-		get_tree().paused = not get_tree().paused
-		#instantiate this scene atop the scene
-		get_tree().change_scene_to_file("res://twilight-city/scenes/menus/gameOver.tscn")
-		
-	
-	#might need to add a bunch of logic and what not for interaction...might be it's own function set(s)
-	if unit == "":
-		pass
-	
