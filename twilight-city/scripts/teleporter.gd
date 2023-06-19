c0b24d216a184d12f426cf27a4d1b642ea4ba951
Jack <jacklair1993@gmail.com>
2023-06-18 22:43:51 -0700

    Gitignore adds, updated comments on teleporter.gd

--- twilight-city/scripts/teleporter.gd
+++ twilight-city/scripts/teleporter.gd
@@ -6,10 +6,13 @@ var cooldown = 5.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# This is hella janky, but in order to get the other teleporter that this one swaps its cargo to, you have to create a metadata
+	# object on each teleporter in the editor, type NodePath, pointing to the other teleporter, and you specifically have to name
+	# that metadata "PartnerPath".  I'm sure there's a better way to link two objects together, this seems unsustainable.
 	var partnerString = get_meta("PartnerPath")
 	partnerObject = get_node(partnerString)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# Using Physics Process because swapping two hitboxes on normal process led to some jankiness.
 func _physics_process(delta):
 	var cdp = 1.0 - (max(0, timer) / cooldown) #cooldownPercentage
 	get_node("Sprite2D").modulate = Color(cdp, cdp, cdp, 1)
